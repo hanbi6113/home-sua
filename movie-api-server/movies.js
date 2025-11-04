68d889f8a6a48bc1cad716e7321ad0dca34e81d5
hanbi6113 <hanbi6113@gmail.com>
2025-11-04 11:55:11 +0900

    SDGs

---
+++ movie-api-server/movies.js
@@ -0,0 +1,40 @@
+export const movies = [
+    {
+        id: 1,
+        title:' 스즈메의 문단속',
+        poster: "https://placehold.co/200x300.png?text=Wonka",
+        genre: "애니메이션",
+        director: "신카이",
+        releaseDate: "2023-03-08",
+        description: "세상에서 가장 달콤란 여정, 좋은 일은 꿈에서부터 시작된다"
+    },
+    {
+        id: 2,
+        title:' 웡카',
+        poster: "https://placehold.co/200x300.png?text=Wonka",
+        genre: "판타지, 드라마",
+        director: "폴 킹",
+        releaseDate: "2024-01-31",
+        description: "세상에서 가장 달콤란 여정, 좋은 일은 꿈에서부터 시작된다"
+    },
+    {
+        id: 3,
+        title:' 웡카',
+        poster: "https://placehold.co/200x300.png?text=Wonka",
+        genre: "판타지, 드라마",
+        director: "폴 킹",
+        releaseDate: "2024-01-31",
+        description: "세상에서 가장 달콤란 여정, 좋은 일은 꿈에서부터 시작된다"
+    },
+    {
+        id: 4,
+        title:' 웡카',
+        poster: "https://placehold.co/200x300.png?text=Wonka",
+        genre: "판타지, 드라마",
+        director: "폴 킹",
+        releaseDate: "2024-01-31",
+        description: "세상에서 가장 달콤란 여정, 좋은 일은 꿈에서부터 시작된다"
+    },
+]
+
+module.exports = movies;
